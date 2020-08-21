a3e37e754d4c887797824ec1d6168894ad6ba687
Ruth <Ruthdlc98@hotmail.com>
2020-08-21 12:51:11 -0600

    Version 1.0.0.

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -5,7 +5,9 @@ import  {todoList} from '../index'
 //Referencias  en el HTML 
 const divTodoList = document.querySelector('.todo-list');
 const txtInput    = document.querySelector ('.new-todo');
-const btnBorrar= document.querySelector('.clear-completed');
+const btnBorrar = document.querySelector('.clear-completed');
+const ulFiltros = document.querySelector('.filters');
+const anchorFiltros = document.querySelectorAll('.filtro');
 
 
 export const creartodoHtml = ( todo ) =>{
@@ -66,4 +68,30 @@ btnBorrar.addEventListener('click', ( event ) => {
         }
     }
     console.log(todoList);
+});
+
+ulFiltros.addEventListener('click', (event) => {
+    const filtro = event.target.text;
+    if( !filtro ){
+        return;
+    }
+
+    anchorFiltros.forEach( elem => elem.classList.remove('selected'));
+    event.target.classList.add('selected');
+    for( const elemento of divTodoList.children){
+        elemento.classList.remove('hidden');
+        const completado = elemento.classList.contains('completed');
+        switch( filtro ){
+            case 'Pendientes': 
+                if (completado) {
+                    elemento.classList.add('hidden');
+                }
+            break;
+            case 'Completados': 
+                if (!completado) {
+                    elemento.classList.add('hidden');
+                }
+            break;
+        }
+    }
 });
